afc5014b28b4b1b1cc6aa55e4459c4b58c964afc
Camilo <kamiloandres2005118@gmail.com>
2024-08-15 14:09:48 -0500

    se agrego el formato para pesos colombianos en los precios de los platillos

--- Adiministrador web/admin_web_oso_montana/src/components/ui/Platillo.jsx
+++ Adiministrador web/admin_web_oso_montana/src/components/ui/Platillo.jsx
@@ -1,16 +1,15 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef } from 'react';
 import { FirebaseContext } from '../../firebase';
 
-const Platillo = ({platillo}) => {
+const Platillo = ({ platillo }) => {
 
     // Existencia ref para acceder al valor directamente
     const existenciaRef = useRef(platillo.existencia);
 
     // context de firebase para cambios en la BD
-    const { firebase } = useContext(FirebaseContext)
+    const { firebase } = useContext(FirebaseContext);
 
-    const { id,  nombre, imagen, existencia, categoria, precio, descripcion } = platillo;
-
+    const { id, nombre, imagen, existencia, categoria, precio, descripcion } = platillo;
 
     // modificar el estado del platillo en firebase
     const actualizarDisponibilidad = () => {
@@ -27,22 +26,32 @@ const Platillo = ({platillo}) => {
         }
     }
 
-    return ( 
+    // Formatear el precio en pesos colombianos
+    const formatearPrecio = (precio) => {
+        return precio.toLocaleString('es-CO', { 
+            style: 'currency', 
+            currency: 'COP',
+            minimumFractionDigits: 0, // No mostrar dígitos decimales mínimos
+            maximumFractionDigits: 0  // No mostrar dígitos decimales máximos
+        });
+    };
+
+    return (
         <div className="w-full px-3 mb-4">
             <div className="p-5 shadow-md bg-white">
                 <div className="lg:flex">
                     <div className="lg:w-5/12 xl:w-3/12">
-                        <img src={imagen} alt=" imagen platillo " />
+                        <img src={imagen} alt="imagen platillo" />
 
                         <div className="sm:flex sm:-mx-2 pl-2">
                             <label className="block mt-5 sm:w-2/4">
-                                <span className="block text-gray-800 mb-2 " >Existencia</span>
+                                <span className="block text-gray-800 mb-2">Existencia</span>
 
                                 <select 
-                                    className="bg-white shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline  "
+                                    className="bg-white shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
                                     value={existencia}
                                     ref={existenciaRef}
-                                    onChange={ () => actualizarDisponibilidad() }
+                                    onChange={() => actualizarDisponibilidad()}
                                 >
                                     <option value="true">Disponible</option>
                                     <option value="false">No Disponible</option>
@@ -51,20 +60,20 @@ const Platillo = ({platillo}) => {
                         </div>
                     </div>
                     <div className="lg:w-7/12 xl:w-9/12 pl-5">
-                        <p className="font-bold text-2xl text-yellow-600 mb-4">{nombre} </p>
+                        <p className="font-bold text-2xl text-yellow-600 mb-4">{nombre}</p>
                         <p className="text-gray-600 mb-4">Categoría: {''}
-                            <span className="text-gray-700 font-bold">{categoria.toUpperCase() }</span> 
+                            <span className="text-gray-700 font-bold">{categoria.toUpperCase()}</span>
                         </p>
-                        <p className="text-gray-600 mb-4">{descripcion} </p>
+                        <p className="text-gray-600 mb-4">{descripcion}</p>
 
                         <p className="text-gray-600 mb-4">Precio: {''}
-                            <span className="text-gray-700 font-bold">$ {precio}</span> 
+                            <span className="text-gray-700 font-bold">{formatearPrecio(precio)}</span>
                         </p>
                     </div>
                 </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Platillo;
